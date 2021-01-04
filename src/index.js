e24ed4318c3a715f6702bc0734d162d9b583d8fb
bbibbibiq <peerasorn14@gmail.com>
2021-01-05 00:09:21 +0700

    [WIP] Add lyric

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+import lyric from "../media/location-unknown/lyric.json";
+
 console.log("hello world!");
 
 const body = document.querySelector("body");
+const audioSound = document.getElementById("audio-sound");
 
 body.addEventListener(
   "click",
   function () {
-    const audioSound = document.getElementById("audio-sound");
     audioSound.play();
-    console.log(1);
+    start();
   },
   false
 );
 
 (() => {})();
+
+function start() {
+  setInterval(function () {
+    const currTime = audioSound.currentTime.toFixed(0);
+    // console.log(currTime);
+    for (let l in lyric) {
+      // console.log(1);
+      if (currTime == l) {
+        console.log(lyric[l].text);
+      }
+    }
+  }, 1000);
+}
