463f44a50ba4de750af5e973fcf13e5345d41c53
bbibbibiq <peerasorn14@gmail.com>
2021-02-06 11:26:57 +0700

    Add caffeine

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,5 @@
 import lyric from "../media/location-unknown/lyric.json";
+import { drink } from "./caffeine";
 
 // type
 type LyricObject = {
@@ -26,6 +27,8 @@ const playSvg = `<svg role="img" height="16" width="16" viewBox="0 0 16 16"><pat
 const pauseSvg = `<svg class="self-center" role="img" height="16" width="16" viewBox="0 0 16 16"><path fill="none" d="M0 0h16v16H0z"></path><path fill="#b3b3b3" d="M3 2h3v12H3zM10 2h3v12h-3z"></path></svg>`;
 
 window.onload = () => {
+  drink();
+
   const { duration } = audioSound;
   const durationTime = toMinSec(duration);
   durationTimeSpan.innerHTML = durationTime;
